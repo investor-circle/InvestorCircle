bc354f5b9fb982a6cfda18516c2ff6b542fc0e4a
myinvestorcircle <myinvestorcircle@users.noreply.github.com>
2026-09-19 06:06:25 +0000

    deploy: b943416096d0104a0f0795ee595bd5f00a46f9ce

--- assets/arrow-left-Dh9yOkPk.js
+++ assets/arrow-left-Dh9yOkPk.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-i410xUWW.js";/**
+import{c as e}from"./index-B8x8AAE_.js";/**
  * @license lucide-react v0.383.0 - ISC
  *
  * This source code is licensed under the ISC license.
